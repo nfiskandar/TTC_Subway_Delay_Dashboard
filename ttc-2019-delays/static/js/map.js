4fa9de6e0a7af692ac38ccd66e355ea5a7b1a8e3
Jay Liu <57325576+superjay5@users.noreply.github.com>
2020-02-10 11:23:01 -0500

    code done, need to check whether ok to run with flask data

--- ttc-2019-delays/static/js/map.js
+++ ttc-2019-delays/static/js/map.js
@@ -20,34 +20,137 @@ var Map ="../data/station_in_line.csv";
 d3.csv(Map).then(function(data){
   console.log(data);	
   
-data.forEach( function(row){
-   try{var coordinates = [parseFloat(row.latitude), parseFloat(row.longitude)];}
-   catch(err){console.log(error
-   )}
-   finally{
-    console.log(coordinates);
-    console.log(row.station);
+// data.forEach( function(row){
+//    try{var coordinates = [parseFloat(row.latitude), parseFloat(row.longitude)];}
+//    catch(err){console.log(error
+//    )}
+//    finally{
+//     console.log(coordinates);
+//     console.log(row.station);
  
-   //circle on the stop
-  var circle = L.circle( coordinates, {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 50
-  }).addTo(mymap);
+//    //circle on the stop
+//   var circle = L.circle( coordinates, {
+//     color: 'red',
+//     fillColor: '#f03',
+//     fillOpacity: 0.5,
+//     radius: 50
+//   }).addTo(mymap);
+//   //popup
+//   circle.bindPopup("<h3>" + row.station +
+//   "</h3><hr><p>" + row.num_delays + 
+//   "</p><hr><p> MAG:" + row.avg_delay_time + "</p>"); 
+// }   
+
+// });
+
+
+// //line1
+// var line1coordinates=[];
+// for (var  a=0; a<40;  a++){
+//   var circle1 = L.circle( [parseFloat(data[a].latitude), parseFloat(data[a].longitude)], {
+//         color: 'red',
+//         fillColor: 'yellow',
+//         fillOpacity: 0.7,
+//         radius: 50
+//       }).addTo(mymap);
+//   //popup
+//   circle1.bindPopup("<h3>" + data[a].station +
+//   "</h3><hr><p>Total Delay time:" + data[a].num_delays + 
+//   "</p><hr><p> Avg Delay time:" + data[a].avg_delay_time + "</p>"); 
+//   // polyline to connect stops  
+//   var latlng1 =[ [parseFloat(data[a].latitude), parseFloat(data[a].longitude)]];
+//   line1coordinates.push(latlng1)  
+//   console.log(data[a].station);
+// };
+// console.log(line1coordinates);
+
+// //line connecting between station
+// var polyline1 = L.polyline(line1coordinates, {
+//   color: "yellow",
+//   weight: 55,
+//   stroke: true
+// }).addTo(mymap);
+
+
+//line 2
+var line2coordinates =[];
+for (var i=40; i <70; i++) {
+  var circle2 = L.circle( [parseFloat(data[i].latitude), parseFloat(data[i].longitude)], {
+        color: 'red',
+        fillColor: 'green',
+        fillOpacity: 0.5,
+        radius: 50
+      }).addTo(mymap);
   //popup
-  circle.bindPopup("<h3>" + row.station +
-  "</h3><hr><p>" + row.num_delays + 
-  "</p><hr><p> MAG:" + row.avg_delay_time + "</p>"); 
-}   
+  circle2.bindPopup("<h3>" + data[i].station +
+  "</h3><hr><p>Total Delay time:" + data[i].num_delays + 
+  "</p><hr><p> Avg Delay time:" + data[i].avg_delay_time + "</p>"); 
+  // polyline to connect stops  
+  var latlng2 =[ [parseFloat(data[i].latitude), parseFloat(data[i].longitude)]];
+  line2coordinates.push(latlng2)  
+  console.log(data[i].station);
+};
+console.log(line2coordinates);
+var polyline2 = L.polyline(line2coordinates, {
+  color: "green",
+  weight: 55,
+  stroke: true
+}).addTo(mymap);
+
+
+//line3
+var line3coordinates=[];
+for (var j =70; j <77; j ++){
+  var circle3 = L.circle( [parseFloat(data[j].latitude), parseFloat(data[j].longitude)], {
+        color: 'red',
+        fillColor: 'blue',
+        fillOpacity: 0.7,
+        radius: 50
+      }).addTo(mymap);
+  //popup
+  circle3.bindPopup("<h3>" + data[j].station +
+  "</h3><hr><p>Total Delay time:" + data[j].num_delays + 
+  "</p><hr><p> Avg Delay time:" + data[j].avg_delay_time + "</p>"); 
+  // polyline to connect stops  
+  var latlng3 =[ [parseFloat(data[j].latitude), parseFloat(data[j].longitude)]];
+  line3coordinates.push(latlng3)  
+  console.log(data[j].station);
+};
+
+console.log(line3coordinates);
+var polyline3 = L.polyline(line3coordinates, {
+  color: "blue",
+  weight: 55,
+  stroke: true
+}).addTo(mymap);
+
+//line4
+var line4coordinates=[];
+for (var k =77; k<82;  k++){
+  var circle4 = L.circle( [parseFloat(data[k].latitude), parseFloat(data[k].longitude)], {
+        color: 'red',
+        fillColor: 'purple',
+        fillOpacity: 0.7,
+        radius: 50
+      }).addTo(mymap);
+  //popup
+  circle4.bindPopup("<h3>" + data[k].station +
+  "</h3><hr><p>Total Delay time:" + data[k].num_delays + 
+  "</p><hr><p> Avg Delay time:" + data[k].avg_delay_time + "</p>"); 
+  // polyline to connect stops  
+  var latlng4 =[ [parseFloat(data[k].latitude), parseFloat(data[k].longitude)]];
+  line3coordinates.push(latlng4)  
+  console.log(data[k].station);
+};
+console.log(line4coordinates);
+
+var polyline4 = L.polyline(line4coordinates, {
+  color: "purple",
+  weight: 55,
+  stroke: true
+}).addTo(mymap);
 
 });
-
-
-
-
-});
-
 
   
 
@@ -57,11 +160,3 @@ data.forEach( function(row){
 
 
  
-
-// polygon to connect stops
-var polygon = L.polygon([
-  [51.509, -0.08],
-  [51.503, -0.06],
-  [51.51, -0.047]
-]).addTo(mymap);
-polygon.bindPopup("I am a polygon.");
